a0bb971fa3a6386be4fe87772174a0d2660eabb0
Masatoshi Furukawa <furukawa@fullness.co.jp>
2026-04-17 16:46:21 +0900

    Java Webアプリケーション ビルドの自動化

--- src/test/java/com/example/java_actions_app/JavaActionsAppApplicationTests.java
+++ src/test/java/com/example/java_actions_app/JavaActionsAppApplicationTests.java
@@ -1,11 +1,11 @@
 package com.example.java_actions_app;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Disabled;
 
 @Disabled
-@SpringBootTest
+//@SpringBootTest
 class JavaActionsAppApplicationTests {
 
 	@Test
